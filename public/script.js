9e923d96aac718861f589c723865fe512d87888c
Koen <kvdekker@hotmail.nl>
2023-05-10 18:03:56 +0200

    Changed layout for modernisation

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navButton = document.querySelector(".navbutton");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navButton.classList.toggle("active");
+});
