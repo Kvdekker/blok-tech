02657ec192a5f84ee71127bc7f55f2ad9012d920
Koen <kvdekker@hotmail.nl>
2023-05-12 18:21:21 +0200

    Added login page with working login/ logout route

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+const session = require("express-session");
 
 const ejs = require("ejs");
 
 const compression = require("compression");
 
 // Eerst alle uses doen
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "geheim-woord",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use((req, res, next) => {
+  res.locals.isLoggedIn = req.session.isLoggedIn;
+  next();
+});
+
 app.use(compression());
 app.use(express.static("public"));
 
@@ -16,12 +31,40 @@ app.set("partials", "partials");
 
 // Homepagina
 app.get("/home", (req, res) => {
+  const isLoggedIn = req.session.isLoggedIn;
   res.render("index");
 });
 
 // login
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get("/loginpagina", (req, res) => {
+  res.render("loginpagina");
+});
+
+// login route
+app.post("/login", (req, res) => {
+  const gebruikersnaam = req.body.gebruikersnaam;
+  const wachtwoord = req.body.wachtwoord;
+
+  if (req.session.isLoggedIn) {
+    return res.redirect("/home");
+  }
+  if (gebruikersnaam === "admin" && wachtwoord === "password") {
+    req.session.isLoggedIn = true; //Sla inlog op
+    res.redirect("/home"); //Stuur naar home
+  } else {
+    res.send("Verkeerde inloggegevens");
+  }
+});
+
+//User pagina
+app.get("/user", (req, res) => {
+  res.render("user");
+});
+
+//Logout
+app.post("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/home");
 });
 
 // Error 404
@@ -32,7 +75,7 @@ app.use(function (req, res) {
 
 // Render errorpage
 app.get("/error", (req, res) => {
-  res.send("<h1>ERROR 404 NOT FOUND</h1>");
+  res.render("<h1>ERROR 404 NOT FOUND</h1>");
 });
 
 // Server has started text
